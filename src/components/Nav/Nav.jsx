4971e40750d9d75f9beefa6c356aab2fdc69f589
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2020-07-16 05:33:32 -0400

    Responsive & separated Hero into new component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -11,7 +11,7 @@ export default function Nav({ handleSubmit }) {
             src="https://upload.wikimedia.org/wikipedia/en/thumb/5/56/NYC_Health.svg/1200px-NYC_Health.svg.png"
             alt=""
           />
-          <h2 className="mx-2 uppercase font-bold text-blue-700">
+          <h2 className="hidden lg:block mx-2 uppercase font-bold text-blue-700">
             Restaurant Violations
           </h2>
         </a>
