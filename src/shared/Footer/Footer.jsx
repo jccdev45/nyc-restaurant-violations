eba3f7eb9fdef3e9c6ce7b49dab36a788bd6679e
Jordan Cruz-Correa <43935515+jccdev45@users.noreply.github.com>
2024-09-18 20:08:30 -0400

    Update Footer.jsx

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="flex flex-col items-center">
         <span>Creation & Design:</span>
         <span> Jordan Cruz-Correa</span>
-        <span>&copy; {new Date().getFullYear()}</span>
+        <span>&copy; 2021</span>
       </div>
     </footer>
   );
